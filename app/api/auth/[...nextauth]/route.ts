f7291e9c2403a6a90f8b829f4a442850aaf951de
ronny11qw <mohammedkhan9271@gmail.com>
2024-10-18 13:18:43 +0530

    Updated with Google Authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,31 @@
+// pages/api/auth/[...nextauth].js
+import { Session } from "inspector/promises";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-
+import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
     providers: [
-        GithubProvider({
-            clientId: 'Ov23liM1nPwSnHlkDnTc',
-            clientSecret:'9d97936b31ae4935ab0c5b444da205d036a78376',
-        })
-    ]
-} 
+       
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+          }),
+    ],
+
+    session: {
+        jwt: true,
+        maxAge: 1 * 60,
+
+    },
+    callbacks: {
+        async redirect({ url, baseUrl }) {
+            // Redirect to the desired page after sign-in
+            return baseUrl + '/dashboard'; // Change '/dashboard' to your desired path
+        },
+    },
+    secret: process.env.NEXTAUTH_SECRET, // Add the secret here
+};
 
 export const handler = NextAuth(authOptions);
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
